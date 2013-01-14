c4a67c6bb9d3503d56bcba7192e4045621e4507c
Sergey Mikhtonyuk <mikhtonyuk@gmail.com>
2013-01-14 12:22:14 +0200

    extended utils

--- hk2/extensions/utils.py
+++ hk2/extensions/utils.py
@@ -1,9 +1,9 @@
 
 #===========================================================
 
-def create_all_extensions(mgr, ep):
+def create_all_extensions(mgr, ep, pred = None):
     """ Returns generator of (extension, instance) tuples for specified extension points """
-    inst = [ (ext, mgr.createInstance(ext)) for ext in ep.extensions() ]
+    inst = [ (ext, mgr.createInstance(ext)) for ext in ep.extensions() if not pred or pred(ext) ]
     return [ i for i in inst if i[1] is not None ]
 
 #===========================================================
