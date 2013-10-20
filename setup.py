7285f6045c4dc2c7b4d9b849fd890e039cb88eaa
Sergii Mikhtoniuk <mikhtonyuk@gmail.com>
2013-10-20 16:44:00 +0300

    exclude tests

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='pyhk2',
       author='Sergii Mikhtoniuk',
       author_email='mikhtonyuk@gmail.com',
       license='MIT',
-      packages=['hk2', 'hk2.annotations', 'hk2.extensions', 'hk2.injection', 'hk2.kernel', 'hk2.types', 'hk2.utils', 'hk2.extensions.impl', 'hk2.kernel.plugin_loaders', 'hk2.extensions.impl.declarative'],
+      packages=find_packages(exclude=['test*']),
       zip_safe=False,
       test_suite='test')
